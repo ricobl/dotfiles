001a9410be70809a2ba605807fc20d8db712a56d
Enrico <rico@rix.(none)>
2009-10-08 00:16:35 -0300

    Improved vim plugins installation

--- vim/plugins.sh
+++ vim/plugins.sh
@@ -1,7 +1,21 @@
 #!/bin/bash
 
-# Installs snipmate
+# Go to .vim user directory
+cd ~/.vim
+
+# Clone the git repo and remove git control files and helpers
 git clone git://github.com/msanders/snipmate.vim.git snipmate
 rm -rf snipmate/.git
-rsync -r snipmate/ .vim/
+rm -f snipmate/convertSnip.py
+# Remove snipmate's snippets so they don't overwrite existent ones
+rm -rf snipmate/.git
+
+# Copies new files recursively
+cp -rfu snipmate/* ./
+
+# Cleanup the clone dir
 rm -rf snipmate
+
+# Updates vim help tags
+vim "+helptags ~/.vim/doc" "+q"
+
