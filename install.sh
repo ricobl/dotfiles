708b1204d3449bd923af5d4e0777acd7dbe0ef35
Enrico <enrico@orangotoe.com.br>
2009-10-08 09:54:28 -0300

    Added vim config to installation script

--- install.sh
+++ install.sh
@@ -5,6 +5,8 @@
 [ ! -d ~/bin ] && ln -s ~/dotfiles/bin ~/bin
 [ ! -f ~/.bash_aliases ] && ln -s ~/dotfiles/bash/.bash_aliases ~/.bash_aliases
 [ ! -f ~/.bashrc ] && ln -s ~/dotfiles/bash/.bashrc ~/.bashrc
+[ ! -f ~/.vimrc ] && ln -s ~/dotfiles/vim/.vimrc ~/.vimrc
+[ ! -d ~/.vim ] && ln -s ~/dotfiles/vim/.vim ~/.vim
 
 # Set git to use meld for diff
 git config --global diff.external ~/bin/git-meld.py
