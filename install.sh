54d97bf80490be4bafde3a9f5a96555de10eb1c0
Enrico <enrico@orangotoe.com.br>
2009-11-20 10:40:52 -0200

    Added git log alias for quick and short logs

--- install.sh
+++ install.sh
@@ -22,3 +22,4 @@ git config --global alias.st status
 git config --global alias.ci commit
 git config --global alias.co checkout
 git config --global alias.br branch
+git config --global alias.lg log --pretty=oneline -n 10
