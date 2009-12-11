3a8a0fc29c330c07741af154cdd665ebc1cdd52a
Enrico <rico@rix.(none)>
2009-12-11 00:00:44 -0200

    Fixed installation script to allow different installation path

--- install.sh
+++ install.sh
@@ -2,11 +2,14 @@
 
 # Installs the dotfiles and helpers
 
-[ ! -d ~/bin ] && ln -s ~/dotfiles/bin ~/bin
-[ ! -f ~/.bash_aliases ] && ln -s ~/dotfiles/bash/.bash_aliases ~/.bash_aliases
-[ ! -f ~/.bashrc ] && ln -s ~/dotfiles/bash/.bashrc ~/.bashrc
-[ ! -f ~/.vimrc ] && ln -s ~/dotfiles/vim/.vimrc ~/.vimrc
-[ ! -d ~/.vim ] && ln -s ~/dotfiles/vim/.vim ~/.vim
+# Get root
+DOTFILES=`dirname $0 | xargs readlink -f`
+
+[ ! -d ~/bin ] && ln -s "$DOTFILES/bin" ~/bin
+[ ! -f ~/.bash_aliases ] && ln -s "$DOTFILES/bash/.bash_aliases" ~/.bash_aliases
+[ ! -f ~/.bashrc ] && ln -s "$DOTFILES/.bashrc" ~/.bashrc
+[ ! -f ~/.vimrc ] && ln -s "$DOTFILES/vim/.vimrc" ~/.vimrc
+[ ! -d ~/.vim ] && ln -s "$DOTFILES/vim/.vim" ~/.vim
 
 # Set git to use meld for diff
 git config --global diff.external ~/bin/git-meld.py
