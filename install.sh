cb08a5dddd301bc21804133ec42392dbf95bfce8
Enrico <rico@rix.(none)>
2009-12-14 20:29:44 -0200

    Added tracking as default to push command

--- install.sh
+++ install.sh
@@ -26,3 +26,4 @@ git config --global alias.ci commit
 git config --global alias.co checkout
 git config --global alias.br branch
 git config --global alias.lg log --pretty=oneline -n 10
+git config --global push.default tracking
