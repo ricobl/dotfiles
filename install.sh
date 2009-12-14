e797f4f48dca72751a870ea4365bbbf06d50bc22
Enrico <rico@rix.(none)>
2009-12-14 20:59:12 -0200

    Fixed .bashrc path and lg (short log) alias

--- install.sh
+++ install.sh
@@ -7,7 +7,7 @@ DOTFILES=`dirname $0 | xargs readlink -f`
 
 [ ! -d ~/bin ] && ln -s "$DOTFILES/bin" ~/bin
 [ ! -f ~/.bash_aliases ] && ln -s "$DOTFILES/bash/.bash_aliases" ~/.bash_aliases
-[ ! -f ~/.bashrc ] && ln -s "$DOTFILES/.bashrc" ~/.bashrc
+[ ! -f ~/.bashrc ] && ln -s "$DOTFILES/bash/.bashrc" ~/.bashrc
 [ ! -f ~/.vimrc ] && ln -s "$DOTFILES/vim/.vimrc" ~/.vimrc
 [ ! -d ~/.vim ] && ln -s "$DOTFILES/vim/.vim" ~/.vim
 
@@ -28,5 +28,5 @@ git config --global alias.st status
 git config --global alias.ci commit
 git config --global alias.co checkout
 git config --global alias.br branch
-git config --global alias.lg log --pretty=oneline -n 10
+git config --global alias.lg log "--pretty=oneline -n 10"
 git config --global push.default tracking
