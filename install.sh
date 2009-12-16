b72685343c765e15b88cf3cc86c716cd777501a8
Enrico <enrico@orangotoe.com.br>
2009-12-16 11:56:26 -0200

    Fixed git lg alias for short logs

--- install.sh
+++ install.sh
@@ -28,5 +28,6 @@ git config --global alias.st status
 git config --global alias.ci commit
 git config --global alias.co checkout
 git config --global alias.br branch
-git config --global alias.lg log "--pretty=oneline -n 10"
+git config --global alias.lg "log --pretty=oneline -n 10"
 git config --global push.default tracking
+git config --global merge.tool meld
