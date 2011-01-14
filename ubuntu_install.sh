8ed4cd95b6785bfb798d30fe9ac94f58920c9448
Enrico <rico.bl@gmail.com>
2011-01-14 13:53:27 -0200

    Slightly improved ubuntu installation script.

--- ubuntu_install.sh
+++ ubuntu_install.sh
@@ -1,36 +1,39 @@
 #!/bin/bash
 
 # Git, vim, meld, pidgin, easy_install, etc...
-sudo apt-get install git-core vim-gnome meld pidgin python-setuptools gnome-gmail-notifier gnome-games
+sudo apt-get install -y git git-core vim-gnome meld python-pip gnome-gmail-notifier
+sudo apt-get install -y cython python-dev libxml2-dev libxslt1-dev
 
 # Install MS Fonts and reload
-sudo apt-get install msttcorefonts 
+sudo apt-get install -y msttcorefonts 
 sudo fc-cache -fv
 
 # Install nautilus extension to open terminal and reload
-sudo apt-get install nautilus-open-terminal
+sudo apt-get install -y nautilus-open-terminal
 nautilus -q
 
 # Google Chrome
 wget https://dl-ssl.google.com/linux/direct/google-chrome-stable_current_i386.deb
-dpkg -i google-chrome-stable_current_i386.deb
+sudo dpkg -i google-chrome-stable_current_i386.deb
 rm google-chrome-stable_current_i386.deb
 
 # Install dotfiles
-mkdir projects
-pushd projects
-git clone git@github.com:ricobl/dotfiles.git
-./dotfiles/install.sh
-popd
+if [ ! -d ~/projects/dotfiles ]; then
+    mkdir -p projects
+    pushd projects
+    git clone git@github.com:ricobl/dotfiles.git
+    ./dotfiles/install.sh
+    popd
+    
+fi
 
 # Pip / Virtualenv / Fabric / ...
-sudo easy_install pip
 sudo pip install -r projects/dotfiles/pip_requirements.txt
-mkdir ~/.virtualenvs
+mkdir -p ~/.virtualenvs
 
 # Codecs
 sudo wget --output-document=/etc/apt/sources.list.d/medibuntu.list http://www.medibuntu.org/sources.list.d/$(lsb_release -cs).list && sudo apt-get --quiet update && sudo apt-get --yes --quiet --allow-unauthenticated install medibuntu-keyring && sudo apt-get --quiet update
-sudo aptitude -y install w32codecs libdvdcss2 ubuntu-restricted-extras
+sudo apt-get install -y w32codecs libdvdcss2 ubuntu-restricted-extras
 
 # JDownloader
 sudo add-apt-repository ppa:jd-team/jdownloader
