976f4aaa02ac0ca7799904417bf136dd9efb24e7
Enrico <rico.bl@gmail.com>
2011-02-12 03:38:44 -0200

    Added keyboard shortcuts.

--- install.sh
+++ install.sh
@@ -40,16 +40,28 @@ git config --global alias.br branch
 git config --global alias.lg "log --format='%Cgreen%h %Cred%cr %Creset%s %Cblue%cn'"
 
 # Gnome Keyboard Shortcuts
+set_key(){
+    gconftool-2 -t string -s "/apps/metacity/$1" "$2"
+}
 set_command_shortcut(){
-    gconftool-2 -t string -s "/apps/metacity/global_keybindings/run_command_$1" "$2"
-    gconftool-2 -t string -s "/apps/metacity/keybinding_commands/command_$1" "$3"
+    set_key "global_keybindings/run_command_$1" "$2"
+    set_key "keybinding_commands/command_$1" "$3"
 }
 set_gnome_shortcut(){
-    gconftool-2 -t string -s "/apps/metacity/global_keybindings/$1" "$2"
+    set_key "global_keybindings/$1" "$2"
 }
 
 set_command_shortcut '1' '<Mod4>v' "gvim '+cd ~'"
 set_command_shortcut '2' '<Mod4>p' "gnome-terminal -e 'ipython'"
 
 set_gnome_shortcut 'show_desktop' '<Mod4>m'
+set_gnome_shortcut 'run_command_terminal' '<Mod4>t'
+
+set_key 'window_keybindings/toggle_maximized' '<F10>'
+
+set_key 'global_keybindings/switch_to_workspace_left' '<Mod4>,'
+set_key 'global_keybindings/switch_to_workspace_right' '<Mod4>.'
+
+set_key 'window_keybindings/move_to_workspace_left' '<Mod4><'
+set_key 'window_keybindings/move_to_workspace_right' '<Mod4>>'
 
