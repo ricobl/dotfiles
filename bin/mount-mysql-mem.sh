76864deed1f1492cd2d9e32f9d90c30428047636
Enrico <rico.bl@gmail.com>
2011-03-03 14:00:05 -0300

    Added script to copy MySQL data to a memory mount.

---
+++ bin/mount-mysql-mem.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+MEM_DIR="/mnt/ramfs"
+MEM_MYSQL_DIR="$MEM_DIR/mysql"
+FS_TYPE="ramfs"
+MOUNT_SIZE="100m"
+
+MYSQL_DIR="/var/lib/mysql"
+
+sudo /etc/init.d/mysql stop
+
+# Cria mount em memória
+[ ! -d $MEM_DIR ] && sudo mkdir $MEM_DIR
+[ ! `df $MEM_DIR | grep $FS_TYPE | awk '{print $6}'` ] && sudo mount -t "$FS_TYPE" -o size="$MOUNT_SIZE" "$FS_TYPE" "$MEM_DIR"
+
+# Copia banco pra memória
+sudo cp -R $MYSQL_DIR $MEM_DIR
+
+# Corrige permissões
+sudo chown -R mysql:mysql $MEM_MYSQL_DIR
+
+# Reconfigura
+#sudo vim /etc/mysql/my.cnf
+#sudo vim /etc/apparmor.d/usr.sbin.mysqld
+
+# Atualiza apparmor e inicia mysql
+#sudo /etc/init.d/apparmor restart
+sudo /etc/init.d/mysql start
+
