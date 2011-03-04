3f03695ed34b45951869fc0fedb74c2b04c4894e
Enrico <rico.bl@gmail.com>
2011-03-03 23:05:20 -0300

    Added mass-pull command to recursively update projects.

---
+++ bin/mass-pull.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+##################
+# Code # Color   #
+##################
+#  00  # Off     #
+#  30  # Black   #
+#  31  # Red     #
+#  32  # Green   #
+#  33  # Yellow  #
+#  34  # Blue    #
+#  35  # Magenta #
+#  36  # Cyan    #
+#  37  # White   #
+##################
+
+function color {
+    echo "\033[$1;$2m"
+}
+
+c_dir=`color 1 34`
+c_off=`color 0 00`
+
+for repo in `find -type d -name ".git"`
+do
+    pushd "$repo/.." > /dev/null
+        echo -e "${c_dir}`pwd`${c_off}"
+        git pull
+    popd > /dev/null
+done
+
