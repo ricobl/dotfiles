af0932e14480127353656ee325b70baf2b49d57b
Enrico <rico.bl@gmail.com>
2011-05-16 13:59:16 -0300

    Fixed double-whitespace on git lg alias.

--- install.sh
+++ install.sh
@@ -38,7 +38,7 @@ git config --global alias.st status
 git config --global alias.ci commit
 git config --global alias.co checkout
 git config --global alias.br branch
-git config --global alias.lg "log --format='%Cgreen%h %Cred%cr %Creset%s %Cblue%cn %Cgreen%d'"
+git config --global alias.lg "log --format='%Cgreen%h %Cred%cr %Creset%s %Cblue%cn%Cgreen%d'"
 
 # Gnome Keyboard Shortcuts
 set_key(){
