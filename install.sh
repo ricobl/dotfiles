15d438a62b3cda557302c63cff2640b93d85f8df
Enrico <rico.bl@gmail.com>
2012-11-14 11:21:46 -0200

    Added "--follow" to the "git lg" alias.

--- install.sh
+++ install.sh
@@ -51,4 +51,4 @@ git config --global alias.st status
 git config --global alias.ci commit
 git config --global alias.co checkout
 git config --global alias.br branch
-git config --global alias.lg "log --format='%Cgreen%h %Cred%cr %Creset%s %Cblue%cn%Cgreen%d'"
+git config --global alias.lg "log --follow --format='%Cgreen%h %Cred%cr %Creset%s %Cblue%cn%Cgreen%d'"
