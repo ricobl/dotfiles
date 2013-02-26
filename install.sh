7bc6767859876e724f52cccb735bb26b17bfbac8
Enrico <rico.bl@gmail.com>
2013-02-26 00:33:07 -0300

    Refactored symlinks creation on install script.

--- install.sh
+++ install.sh
@@ -6,16 +6,22 @@ realpath() {
     [[ $1 = /* ]] && echo "$1" || echo "$PWD/${1#./}"
 }
 
+safe_link(){
+    local src="$1"
+    local dest="$HOME/`basename $src`"
+    [ ! -e "$dest" ] && ln -sf "$src" "$dest"
+}
+
 # Get root
 SCRIPT_PATH=`realpath $0`
 DOTFILES=`dirname $SCRIPT_PATH`
 
-[ ! -d ~/bin ] && ln -s "$DOTFILES/bin" ~/bin
-[ ! -f ~/.bashrc ] && ln -s "$DOTFILES/dotfiles/.bashrc" ~/.bashrc
-[ ! -f ~/.inputrc ] && ln -s "$DOTFILES/dotfiles/.inputrc" ~/.inputrc
-[ ! -f ~/.vimrc ] && ln -s "$DOTFILES/dotfiles/.vimrc" ~/.vimrc
-[ ! -d ~/.ipython ] && ln -s "$DOTFILES/dotfiles/.ipython" ~/.ipython
-[ ! -d ~/.vim ] && ln -s "$DOTFILES/dotfiles/.vim" ~/.vim
+# Install bin scripts
+safe_link "$DOTFILES/bin"
+# Install other dotfiles
+for f in `ls -A $DOTFILES/dotfiles`; do
+    safe_link "$DOTFILES/dotfiles/$f"
+done
 
 # Updates vim help tags
 vim "+helptags ~/.vim/doc" "+q"
