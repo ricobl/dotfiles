517e092719d927698c0c4441f7aea4196ddcc1f6
Enrico <rico.bl@gmail.com>
2016-07-07 14:44:37 -0300

    Made sort-by-date.sh osx compatible

--- bin/sort-by-date.sh
+++ bin/sort-by-date.sh
@@ -2,8 +2,26 @@
 
 # Distributes files in YEAR/MONTH directories
 
+OS=`uname`
+
+timestamp_osx () {
+    file="$1"
+    echo `stat -f '%Sm' -t '%Y/%m' "$file"`
+}
+
+timestamp_nix () {
+    file="$1"
+    echo `date +%Y/%m -r "$file"`
+}
+
+if [[ $OS == 'Darwin' ]]; then
+    timestamp=timestamp_osx
+else
+    timestamp=timestamp_nix
+fi
+
 for file in "$@"; do
-    dir=`date +%Y/%m -r "$file"`
-    mkdir -vp $dir
-    mv -v "$file" $dir
+    dir=`"$timestamp" "$file"`
+    echo mkdir -vp $dir
+    echo mv -v "$file" "$dir"
 done
