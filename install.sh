96d08689aa2543035226bf9fa7dc30cff8d5a498
Enrico <rico.bl@gmail.com>
2017-03-29 18:36:17 -0300

    Changed refs color on `git lg` alias

--- install.sh
+++ install.sh
@@ -74,4 +74,4 @@ git config --global alias.st status
 git config --global alias.ci commit
 git config --global alias.co checkout
 git config --global alias.br branch
-git config --global alias.lg "log --format='%Cgreen%h %Cred%cd %Creset%s %Cblue%an%Cgreen%d' --date=relative"
+git config --global alias.lg "log --format='%C(green)%h %C(red)%cd %C(reset)%s %C(blue)%an%C(yellow)%d' --date=relative"
