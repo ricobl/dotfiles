53e5b7ed3eed0ef14ed65f4c6c173e636fc6ad4d
Enrico <rico.bl@gmail.com>
2025-07-17 21:23:20 +1000

    Preserve TERM variable when starting test shell

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
 
+home_dir=${1:-$HOME}
+
 # Start a bash shell with a clean environment and preserve the HOME directory
-env -i HOME=$HOME bash --noprofile --rcfile core/home/.bashrc
+env -i \
+  HOME=$home_dir \
+  TERM=$TERM \
+  bash --noprofile --rcfile core/home/.bashrc
