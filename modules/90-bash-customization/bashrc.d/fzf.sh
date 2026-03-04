f4069d10b75a13250cc6f919a5dc456cdba99626
Enrico <rico.bl@gmail.com>
2026-03-04 15:33:06 +1100

    Update fzf key bindings and completion integration

--- modules/90-bash-customization/bashrc.d/fzf.sh
+++ modules/90-bash-customization/bashrc.d/fzf.sh
@@ -1,2 +1,4 @@
 export FZF_DEFAULT_OPTS="--bind ctrl-a:select-all,ctrl-d:deselect-all,ctrl-t:toggle-all"
-[ -f ~/.fzf.bash ] && source ~/.fzf.bash
+
+# Set up fzf key bindings and fuzzy completion
+eval "$(fzf --bash)"
