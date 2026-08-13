4860cfb26b8e0d729c87a92e7366f585271d8f89
Enrico <rico.bl@gmail.com>
2026-08-14 09:48:46 +1000

    Add `~/projects` into CDPATH

--- modules/90-bash-customization/bashrc.d/cd-helpers.sh
+++ modules/90-bash-customization/bashrc.d/cd-helpers.sh
@@ -1,5 +1,5 @@
 # Add work directory to CDPATH path
-[ -d  "$HOME/work" ] && CDPATH="$CDPATH:$HOME/work"
+[ -d  "$HOME/work" ] && CDPATH="$CDPATH:$HOME/work:$HOME/projects"
 
 cdgit(){
   cd "$(git rev-parse --show-toplevel)/$1"
