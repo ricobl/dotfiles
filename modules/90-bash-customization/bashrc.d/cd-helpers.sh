5007fb3fd6c9a84abd6d4f08d52b0bc299f10dd9
Enrico <rico.bl@gmail.com>
2026-08-16 20:15:22 +1000

    Fix ~/projects only being added to CDPATH when ~/work exists

--- modules/90-bash-customization/bashrc.d/cd-helpers.sh
+++ modules/90-bash-customization/bashrc.d/cd-helpers.sh
@@ -1,5 +1,6 @@
 # Add work directory to CDPATH path
-[ -d  "$HOME/work" ] && CDPATH="$CDPATH:$HOME/work:$HOME/projects"
+[ -d "$HOME/work" ] && CDPATH="$CDPATH:$HOME/work"
+[ -d "$HOME/projects" ] && CDPATH="$CDPATH:$HOME/projects"
 
 cdgit(){
   cd "$(git rev-parse --show-toplevel)/$1"
